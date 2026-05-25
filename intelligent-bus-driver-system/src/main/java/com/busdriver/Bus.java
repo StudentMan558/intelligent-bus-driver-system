e5fff7ec1e7ce898058dce8f0ee6a92f3e94b9d2
Adam Dimkovski <s4168373@student.rmit.edu.au>
2026-05-25 17:31:57 +1000

    Added some basic implementation to Bus.java

--- intelligent-bus-driver-system/src/main/java/com/busdriver/Bus.java
+++ intelligent-bus-driver-system/src/main/java/com/busdriver/Bus.java
@@ -4,10 +4,10 @@ package com.busdriver;
 
 /**
  * Represents a bus in the system
- * @author [Team Member Names]
+ * @author Adam Dimkovski (s4168373)
  */
 
-/** 
+
 public class Bus {
     private String busID;
     private int capacity;
@@ -15,12 +15,55 @@ public class Bus {
     private String fuelType; // Diesel, Hybrid, Electricity
 
     // TODO: Add constructors, getters, setters, and validation methods
-}
-*/
-    public class Bus
-    {
-        public static void main( String[] args)
-        {
-            System.out.println("Hello world!");
-        }
+
+    // Basic Constructor (Variable names subject to change)
+    public Bus(String busID, int capacity, double fuelLevel, String fuelType) {
+        setBusID(busID);
+        setCapacity(capacity);
+        setFuelLevel(fuelLevel);
+        setFuelType(fuelType);
     }
+
+    // Getter: Returns busID
+    public String getBusID() {
+        return this.busID;
+    }
+
+    // Setter: Sets/Updates BusID
+    public void setBusID(String busString) {
+        this.busID = busString;
+    }
+
+    // Getter: Returns capacity
+    public int getCapacity() {
+        return this.capacity;
+    }
+
+    // Setter: Sets/Updates Capacity
+    public void setCapacity(int busSpace) {
+        this.capacity = busSpace;
+    }
+
+    // Getter: Returns fuelLevel
+    public double setFuelLevel() {
+        return this.fuelLevel;
+    }
+
+    // Setter: Sets/Updates fuelLevel
+    public void setFuelLevel(double fuelAmount) {
+        this.fuelLevel = fuelAmount;
+    }
+
+    // Getter: Returns fuelType
+    public double setFuelType() {
+        return this.fuelType;
+    }
+
+    // Setter: Sets/Updates FuelType 
+    public void setFuelType(String TypeOfFuel) {
+        this.fuelType = TypeOfFuel;
+    }
+
+}
+
+

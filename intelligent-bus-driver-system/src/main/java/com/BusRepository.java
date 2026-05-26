455b7a9451527c98f28a5dc17c33b7e5a2d43b73
NedSantracRMITSchool <S4170435@student.rmit.edu.au>
2026-05-26 19:00:32 +1000

    drafting of busdriver repository class and sample data in buses.txt

--- intelligent-bus-driver-system/src/main/java/com/BusRepository.java
+++ intelligent-bus-driver-system/src/main/java/com/BusRepository.java
@@ -1,11 +1,43 @@
 package com;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 /**
  * Repository for managing bus data persistence
  * @author [Team Member Names]
  */
 public class BusRepository {
-    private static final String DATA_FILE = "data/buses.txt";
+
+    private static final String FILE_PATH = "buses.txt";
+
+    // create a bus object with only placeholder values
+    private Bus bus;
 
     // TODO: Implement Add(), Update(), Retrieve(), Count() methods
+
+    
+    // collect variables for bus object from data/buses.txt
+    while ((line = reader.readLine()) != null) {
+        try (FileInputStream input = new FileInputStream("data\\buses.txt")) {
+            
+        }
+    }
+
+    //the following updates the bus objects parameters with the correct types
+    setBusID();
+    setCapacity();
+    setFuelLevel();
+    setFuelType();
+
+    //the following returns the number of stored busses 
+    public int getStoredBusAmt() {
+        // read from file and set to variable
+        try (FileInputStream input = new FileInputStream("filename.txt")) {
+
+        }
+        int busAmt;
+        int this.busAmt = // test data from tests
+        return busAmt;  
+    }
+
 }
